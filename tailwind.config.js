dc0859f18b9e47559c9f75176494d75629f22628
pooniajatin <141109606+pooniajatin@users.noreply.github.com>
2024-11-26 23:03:24 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary-col': '#07ecc5',
+        'text-col': '#fffeff',
+        'bg1-col': '#0e1116',
+        'bg2-col': '#151c24'
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
